911b032048e5406eecf86bfde2eac26e2852c688
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2025-08-27 08:54:54 -0300

    feat: Implementado Drawer em menu

--- src/components/DefaultHeader/Menu.tsx
+++ src/components/DefaultHeader/Menu.tsx
@@ -1,10 +1,37 @@
-import { IconButton } from "@chakra-ui/react";
-import { IoIosMenu } from "react-icons/io";
+import { CloseButton, Drawer, IconButton, Kbd, Portal } from "@chakra-ui/react";
+import { IoMdMenu } from "react-icons/io";
 
 export function Menu() {
   return (
-    <IconButton variant="outline">
-      <IoIosMenu />
-    </IconButton>
+    <Drawer.Root size="md" placement="start">
+      <Drawer.Trigger asChild>
+        <IconButton variant="ghost" rounded="full">
+          <IoMdMenu />
+        </IconButton>
+      </Drawer.Trigger>
+
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Header>
+              <Drawer.Title>Henrique Dos Santos Junkes</Drawer.Title>
+            </Drawer.Header>
+
+            <Drawer.Body>
+              Press the <Kbd>esc</Kbd> key to close the drawer.
+            </Drawer.Body>
+
+            <Drawer.Footer>
+            </Drawer.Footer>
+
+            <Drawer.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Drawer.CloseTrigger>
+
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
   );
 }
